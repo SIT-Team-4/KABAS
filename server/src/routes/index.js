8a7cb63990f7c3a88c31577f41077075aac4999a
Khairal Fazil <2504043@sit.singaporetech.edu.sg>
2026-02-16 07:11:48 +0800

    KBAS-35 Convert project from CommonJS to ESM
    Switch all require()/module.exports to import/export syntax.
    Add "type": "module" to package.json and update ESLint config
    to enforce file extensions on relative imports.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import githubRoutes from './github.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json({ message: 'Welcome to the KABAS API' });
 });
-
-router.use('/github', githubRoutes);
 
 export default router;
